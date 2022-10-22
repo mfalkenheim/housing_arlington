b07728247237b9fdddf14cd6dbcfc39a8fb2183e
mfalkenh <mfalkenh@gmail.com>
2022-10-22 10:26:21 -0400

    initial commit

---
+++ src/import_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from urllib import request      
+import gzip
+
+print("hello World")
+
+url = "https://download.data.arlingtonva.us/RealEstate/ImprovementExterior.txt"
+improvement_exterior = pd.read_csv(request.urlopen(url), header=0, sep='|')
+url = "https://download.data.arlingtonva.us/RealEstate/ImprovementDwelling.txt"
+improvement_dwelling = pd.read_csv(request.urlopen(url), header=0, sep='|')
+url = "https://download.data.arlingtonva.us/RealEstate/ImprovementInterior.txt"
+improvement_interior = pd.read_csv(request.urlopen(url), header=0, sep='|')
+url = "https://download.data.arlingtonva.us/RealEstate/Outbuilding.txt"
+outbuilding = pd.read_csv(request.urlopen(url), header=0, sep='|')
+url = "https://download.data.arlingtonva.us/RealEstate/ImprovementPorch.txt"
+porches = pd.read_csv(request.urlopen(url), header=0, sep='|')
+url = "https://download.data.arlingtonva.us/RealEstate/PropertyAddress.txt"
+property_address = pd.read_csv(request.urlopen(url), header=0, sep='|')
+print(property_address.head)
+url = "https://download.data.arlingtonva.us/RealEstate/SalesHistory.txt"
+sales_history = pd.read_csv(request.urlopen(url), header=0, sep='|')
+print(sales_history.head)
+url = "https://download.data.arlingtonva.us/RealEstate/Assessment.txt.gz"
+assessments = pd.read_csv(request.urlopen(url), compression='gzip', header=0, sep='|')
+print(assessments.head)
+url = "https://download.data.arlingtonva.us/HousingBuilding/DevelopmentProject.txt"
+development_projects = pd.read_csv(request.urlopen(url), header=0, sep='|')
+url = "https://download.data.arlingtonva.us/Permit/Permit.txt.gz"
+permits = pd.read_csv(request.urlopen(url), compression = 'gzip', header=0, sep='|')
